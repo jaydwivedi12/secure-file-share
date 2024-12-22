101b87011cc3106af4dc19ffe72c1e9232cc554e
Jay <jay@gmail.com>
2024-12-22 16:18:34 -0500

    url added for logout

--- Backend/Auth_app/urls.py
+++ Backend/Auth_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('register/',views.register,name='register'),
     path('login/',views.login,name='login'),
+    path('logout/',views.logout,name='logout'),
     path('verify2fa/',views.verify_2fa,name='verify2fa'),
 ]
