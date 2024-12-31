a18f8c8c3d4b146548e9518c2502d4faf9de4c77
Jay <jay@gmail.com>
2024-12-31 12:22:51 -0500

    env variable set up

--- Backend/server/settings.py
+++ Backend/server/settings.py
@@ -20,15 +20,16 @@ import dotenv
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-dotenv_file = os.path.join(BASE_DIR, ".env")
-if os.path.isfile(dotenv_file):
-    dotenv.load_dotenv(dotenv_file)
+# dotenv_file = os.path.join(BASE_DIR, ".env")
+# if os.path.isfile(dotenv_file):
+#     dotenv.load_dotenv(dotenv_file)
     
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ["SECRET_KEY"]
+SECRET_KEY = os.getenv("SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
